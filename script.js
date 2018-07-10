43585b049fab8dfa1d11118653c5d00a3e186775
asukaokochi <x17018xx@gmail.com>
2018-07-10 23:03:44 +0900

    作品の追加等々

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
     $('a[href^="#"]').click(function () {
         var speed = 500;
         var href = $(this).attr("href");
@@ -9,4 +9,53 @@ $(function () {
         }, speed, "swing");
         return false;
     });
+    $.fn.pointer = function (options) {
+        var settings = {
+            size: 80,
+            spd: 300,
+            color: "#ccc"
+        }
+        settings = $.extend(settings, options);
+
+        var circle_style = {
+            "position": "absolute",
+            "z-index": 9999,
+            "height": 10,
+            "width": 10,
+            "border": "solid 4px " + settings.color,
+            "border-radius": settings.size
+        }
+        return this.each(function () {
+            var $this = $(this);
+            $this.css({
+                "position": "relative"
+            });
+            $(document).click(function (e) {
+                var x = e.pageX - 5;
+                var y = e.pageY - 5;
+
+                var pos = {
+                    top: (-settings.size / 2) + y,
+                    left: (-settings.size / 2) + x
+                }
+
+                $this.append('<div class="circle"></div>');
+                $this.find(".circle:last").css(circle_style).css({
+                        "top": y,
+                        "left": x
+                    }).animate({
+                        "height": settings.size,
+                        "width": settings.size,
+                        "left": pos.left,
+                        "top": pos.top
+                    }, {
+                        duration: settings.spd,
+                        queue: false
+                    })
+                    .fadeOut(settings.spd * 1.8, function () {
+                        $(this).remove();
+                    });
+            });
+        });
+    }
 });
